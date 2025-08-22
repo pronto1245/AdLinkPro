03a1674f01b658d13d0b1c86160e11bff0e0ecc5
a1 <a1@Macbook-pro1.local>
2025-08-22 22:01:09 +0300

    chore: remove server/db/schema.ts duplicate

--- server/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
-
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  email: text('email').notNull(),
-  username: text('username'),
-  role: text('role'),
-  password_hash: text('password_hash'),
-});
