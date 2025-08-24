bd9abd3e82a8cdd9254927ccd4342dfe4fcd421f
pronto1245 <9791207@gmail.com>
2025-08-24 18:35:30 +0300

    fix: install all missing deps and types

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
