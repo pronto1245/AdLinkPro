455fc45f62db144186fe80b63a7ef016ce23a26e
a1 <a1@Macbook-pro1.local>
2025-08-24 10:24:29 +0300

    feat: новая сборка frontend и обновление public

---
+++ server/public/assets/skeleton-xv0nRF1e.js
@@ -0,0 +1,1 @@
+import{j as m}from"./ui-CkQMVfNu.js";import{a as o}from"./index-Hu68kGEe.js";function a({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{a as S};
